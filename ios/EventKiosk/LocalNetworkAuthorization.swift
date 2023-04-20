813f7118ac92e5bc332293d8252b44a6929815ff
Andrew Dunn <aldunn@aeyon.us>
2023-04-19 20:36:47 -0500

    Edit PRint Size

---
+++ ios/EventKiosk/LocalNetworkAuthorization.swift
@@ -0,0 +1,8 @@
+//
+//  LocalNetworkAuthorization.swift
+//  EventKiosk
+//
+//  Created by Andrew Dunn on 4/18/23.
+//
+
+import Foundation
---
+++ ios/EventKiosk/LocalNetworkPrivacy.m
@@ -0,0 +1,8 @@
+//
+//  LocalNetworkPrivacy.m
+//  EventKiosk
+//
+//  Created by Andrew Dunn on 4/18/23.
+//
+
+#import <Foundation/Foundation.h>
